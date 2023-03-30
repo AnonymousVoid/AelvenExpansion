358eb59a8b5375e6bb7c7932239d8120dc0d9b9b
AlienDog117 <alyn.elephant@gmail.com>
2023-03-29 21:55:25 -0400

    Added the ability to see kiln recipes from kiln block usages in jei, and added an animated progress arrow to the kiln jei plugin.

--- src/main/java/dev/anonymousvoid/aelven_expansion/integration/KilnRecipeCategory.java
+++ src/main/java/dev/anonymousvoid/aelven_expansion/integration/KilnRecipeCategory.java
@@ -1,16 +1,16 @@
 package dev.anonymousvoid.aelven_expansion.integration;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import dev.anonymousvoid.aelven_expansion.AelvenExpansion;
 import dev.anonymousvoid.aelven_expansion.block.ModBlocks;
 import dev.anonymousvoid.aelven_expansion.item.ModItems;
 import dev.anonymousvoid.aelven_expansion.recipe.KilnRecipe;
-import dev.anonymousvoid.aelven_expansion.recipe.ModRecipes;
 import mezz.jei.api.constants.VanillaTypes;
-import mezz.jei.api.gui.builder.IIngredientAcceptor;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
-import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
@@ -27,10 +27,14 @@ public class KilnRecipeCategory implements IRecipeCategory<KilnRecipe> {
 
     private final IDrawable background;
     private final IDrawable icon;
+    private final IDrawableAnimated arrow;
 
     public KilnRecipeCategory(IGuiHelper helper) {
         this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 85);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.KILN.get()));
+
+        arrow = helper.drawableBuilder(TEXTURE, 176, 0, 26, 15)
+                .buildAnimated(240, IDrawableAnimated.StartDirection.LEFT, false);
     }
 
     @Override
@@ -46,10 +50,23 @@ public class KilnRecipeCategory implements IRecipeCategory<KilnRecipe> {
     public IDrawable getIcon() { return this.icon; }
 
     @Override
-    public void setRecipe(IRecipeLayoutBuilder builder, KilnRecipe recipe, IFocusGroup focuses) {
-        builder.addSlot(RecipeIngredientRole.INPUT,20,35).addIngredients(Ingredient.of(new ItemStack(ModItems.ELERIUM.get())));
-        builder.addSlot(RecipeIngredientRole.INPUT,56,17).addIngredients(recipe.getIngredients().get(0));
+    public void draw(KilnRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack stack, double mouseX, double mouseY) {
+//        arrow.draw(stack, 80, 8);
+        arrow.draw(stack, 80, 34);
+//        arrow.draw(stack, 80, 60);
+    }
 
-        builder.addSlot(RecipeIngredientRole.OUTPUT,116,9).addItemStack(recipe.getResultItem());
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, KilnRecipe recipe, IFocusGroup focuses) {
+        builder.addSlot(RecipeIngredientRole.INPUT,20,35).addIngredients(Ingredient.of(new ItemStack(ModItems.ELERIUM.get(), 3)));
+//        builder.addSlot(RecipeIngredientRole.INPUT,56,17).addIngredients(recipe.getIngredients().get(0));
+        builder.addSlot(RecipeIngredientRole.INPUT,56,35).addIngredients(recipe.getIngredients().get(0));
+//        builder.addSlot(RecipeIngredientRole.INPUT,56,53).addIngredients(recipe.getIngredients().get(0));
+
+//        builder.addSlot(RecipeIngredientRole.OUTPUT,116,9).addItemStack(recipe.getResultItem());
+        builder.addSlot(RecipeIngredientRole.OUTPUT,116,35).addItemStack(recipe.getResultItem());
+//        builder.addSlot(RecipeIngredientRole.OUTPUT,116,61).addItemStack(recipe.getResultItem());
+
+        builder.addInvisibleIngredients(RecipeIngredientRole.CATALYST).addItemStack(new ItemStack(ModBlocks.KILN.get()));
     }
 }
