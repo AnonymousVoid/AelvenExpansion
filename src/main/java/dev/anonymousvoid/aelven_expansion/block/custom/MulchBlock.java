64fa578e3fab68c587aa2b77075afef7691fcdf7
AlienDog117 <alyn.elephant@gmail.com>
2023-05-13 11:11:25 -0400

    Peachgrove Swamps and Chalk Peaks should spawn correctly most of the time now. They also have basic surface rules without added features and extra foliage. Mulch Block hitbox fixed to be the same as mud, and visual box to be full block.

--- src/main/java/dev/anonymousvoid/aelven_expansion/block/custom/MulchBlock.java
+++ src/main/java/dev/anonymousvoid/aelven_expansion/block/custom/MulchBlock.java
@@ -12,18 +12,16 @@ import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.lighting.LayerLightEngine;
+import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class MulchBlock extends Block implements BonemealableBlock {
-    protected static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 15.0D, 16.0D);
+    protected static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D);
 
     public MulchBlock(BlockBehaviour.Properties properties) {
         super(properties);
-    }
-
-    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        return SHAPE;
     }
 
     public boolean isValidBonemealTarget(BlockGetter getter, BlockPos pos, BlockState state, boolean b) {
@@ -91,5 +89,21 @@ public class MulchBlock extends Block implements BonemealableBlock {
             }
         }
 
+    }
+
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return SHAPE;
+    }
+
+    public VoxelShape getBlockSupportShape(BlockState p_221566_, BlockGetter p_221567_, BlockPos p_221568_) {
+        return Shapes.block();
+    }
+
+    public VoxelShape getVisualShape(BlockState p_221556_, BlockGetter p_221557_, BlockPos p_221558_, CollisionContext p_221559_) {
+        return Shapes.block();
+    }
+
+    public boolean isPathfindable(BlockState p_221547_, BlockGetter p_221548_, BlockPos p_221549_, PathComputationType p_221550_) {
+        return false;
     }
 }
